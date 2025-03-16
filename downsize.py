bedb3e2cad10f242df1a3b14abf91a09ede15529
Diego Miguel <dmlls@diegomiguel.me>
2025-03-16 16:39:00 +0100

    Format code

--- downsize.py
+++ downsize.py
@@ -3,52 +3,58 @@
 import argparse
 import logging
 import os
-
 from pathlib import Path
+
 from PIL import Image
 
 logger = logging.getLogger(__name__)
 
+
 def get_args() -> dict:
-    argparse.ArgumentParser(description='Downsize an image')
+    argparse.ArgumentParser(description="Downsize an image")
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', type=str, required=True, help='path to image')
-    parser.add_argument('--root-folder', action=argparse.BooleanOptionalAction)
-    parser.add_argument('--log-level', type=str, default='INFO', help='log level')
-    parser.add_argument('--overwrite', action=argparse.BooleanOptionalAction)
+    parser.add_argument("--path", type=str, required=True, help="path to image")
+    parser.add_argument("--root-folder", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--log-level", type=str, default="INFO", help="log level")
+    parser.add_argument("--overwrite", action=argparse.BooleanOptionalAction)
     ret_dict: dict = {
-        'path': parser.parse_args().path,
-        'root-folder': parser.parse_args().root_folder,
-        'log-level': parser.parse_args().log_level,
-        'overwrite': parser.parse_args().overwrite
+        "path": parser.parse_args().path,
+        "root-folder": parser.parse_args().root_folder,
+        "log-level": parser.parse_args().log_level,
+        "overwrite": parser.parse_args().overwrite,
     }
     return ret_dict
 
 
 def downsize(path: str, overwrite: bool = False) -> None:
     img: Image = Image.open(path)
-    img = img.resize((300,300), Image.LANCZOS)
+    img = img.resize((300, 300), Image.LANCZOS)
     if overwrite:
         logger.info(f"Overwriting {path}")
         img.save(path, optimize=True, quality=95)
     else:
         path = Path(path)
-        path = path.parent / Path(path.stem + '_downsized' + path.suffix)
+        path = path.parent / Path(path.stem + "_downsized" + path.suffix)
         logger.info(f"Write {path}")
-        img.save(path, optimize=True, quality=95) 
+        img.save(path, optimize=True, quality=95)
 
 
 def main() -> None:
     args: dict = get_args()
-    path: str = args['path']
-    overwrite: bool = args['overwrite']
+    path: str = args["path"]
+    overwrite: bool = args["overwrite"]
 
-    if args['log-level'] == 'INFO':
+    if args["log-level"] == "INFO":
         logging.basicConfig(level=logging.INFO)
 
-    if bool(args['root-folder']):
+    if bool(args["root-folder"]):
         # TODO: Use pathlib with glob
-        files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(path) for f in filenames if os.path.splitext(f)[1] in ['.jpeg', '.jpg', '.png']]
+        files = [
+            os.path.join(dp, f)
+            for dp, dn, filenames in os.walk(path)
+            for f in filenames
+            if os.path.splitext(f)[1] in [".jpeg", ".jpg", ".png"]
+        ]
         for e in files:
             logger.info(f"Downsizing {e}")
             downsize(e, overwrite)
